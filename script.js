1564a389ff89ef59b673ec15c3487dc7add7f1b7
puroome <129413484+puroome@users.noreply.github.com>
2025-10-09 14:53:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -973,7 +973,7 @@ const learningMode = {
             .filter(s => s.distance < s.word.length / 2 + 1);
     
         if (levenshteinSuggestions.length > 0 || explanationMatches.length > 0) {
-            const title = `'<strong>${startWord}</strong>'에 대한 검색 결과가 없습니다.<br>아래 목록에서 확인해보세요.`;
+            const title = `<strong>${startWord}</strong>(이)가 없으니, 아래에서 확인해보세요.`;
             this.displaySuggestions(levenshteinSuggestions, explanationMatches, title);
         } else {
             const title = `'<strong>${startWord}</strong>'에 대한 검색 결과가 없습니다.`;
@@ -1105,4 +1105,5 @@ const learningMode = {
 document.addEventListener('DOMContentLoaded', () => {
     app.init();
 });
+
 
